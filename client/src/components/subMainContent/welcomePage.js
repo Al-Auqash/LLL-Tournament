5f1d2e8ae387516ece0f15206d44e132bb5faa7b
issyl <ilhamtawakhal@gmail.com>
2021-05-10 10:40:09 +0700

    mainContent #7

--- client/src/components/subMainContent/welcomePage.js
+++ client/src/components/subMainContent/welcomePage.js
@@ -4,14 +4,15 @@ import "./welcomePage.css";
 const welcomePage = () => {
   return (
     <div className="welcomePage">
-      {/* <div className="background"></div> */}
+      {/* <div className="background"> */}
         <h1>JOIN THE COMPETITIVE ESPORT GAMING</h1>
         <h4>CONTEST YOUR SKILL AMONG THE PROS</h4>
         <a href="#" className="btnPosition">
-          <button className="signIn">
+          <button className="joinNow">
             <p className="btnText">Join Now</p>
           </button>
         </a>
+      {/* </div> */}
     </div>
   );
 };
