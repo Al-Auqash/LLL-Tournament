011c0314f06111eddfa16f4ea97aadc05fcfe622
issyl <ilhamtawakhal@gmail.com>
2021-05-12 11:36:16 +0700

    mainContent #8

--- client/src/components/mainContent.js
+++ client/src/components/mainContent.js
@@ -1,11 +1,13 @@
 import React from "react";
 import WelcomePage from "./subMainContent/welcomePage";
+import Works from "./subMainContent/works";
 import "./mainContent.css";
 
 const mainContent = () => {
   return (
     <section id="mainContent">
       <WelcomePage />
+      <Works  />
     </section>
   );
 };
