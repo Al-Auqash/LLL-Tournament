7b24d05bd639478ff2a541ed4af52cb32479c7d5
issyl <ilhamtawakhal@gmail.com>
2021-05-12 12:31:55 +0700

    third content #10

--- client/src/components/mainContent.js
+++ client/src/components/mainContent.js
@@ -1,6 +1,8 @@
 import React from "react";
 import WelcomePage from "./subMainContent/welcomePage";
 import Works from "./subMainContent/works";
+import OurPartners from "./subMainContent/ourPartners";
+
 import "./mainContent.css";
 
 const mainContent = () => {
@@ -8,6 +10,7 @@ const mainContent = () => {
     <section id="mainContent">
       <WelcomePage />
       <Works  />
+      <OurPartners />
     </section>
   );
 };
