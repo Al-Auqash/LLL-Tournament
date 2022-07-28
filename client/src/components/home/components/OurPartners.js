d4f858223d4dab401575795cf30347fdf82b7745
Al-Auqash <ilhamtawakhal@gmail.com>
2022-07-28 22:00:50 +0700

    #61 works page

--- client/src/components/home/components/OurPartners.js
+++ client/src/components/home/components/OurPartners.js
@@ -7,23 +7,23 @@ const OurPartners = () => {
       <div className="title">
         <h1>OUR PARTNERS</h1>
       </div>
-      <div className="ourPartnersCard">
-          <div className="card">
+      <div className="ourPartnersCard grid-wrapper mx-auto">
+          <div className="cs-logo-card">
             <p>Apex</p>
           </div>
-          <div className="card">
+          <div className="cs-logo-card">
             <p>Valorant</p>
           </div>
-          <div className="card">
+          <div className="cs-logo-card">
             <p>Dota 2</p>
           </div>
-          <div className="card">
+          <div className="cs-logo-card">
             <p>PUBG</p>
           </div>
-          <div className="card">
+          <div className="cs-logo-card">
             <p>Rainbow Six Siege</p>
           </div>
-          <div className="card">
+          <div className="cs-logo-card">
             <p>League of Legends</p>
           </div>
       </div>
--- client/src/components/home/components/Works.js
+++ client/src/components/home/components/Works.js
@@ -3,37 +3,37 @@ import "../Homepage.css";
 import Card from "./Card";
 
 const works = () => {
-   return (
-      <div className="works">
-         <div className="title">
-            <h1>HOW GCC WORKS</h1>
-         </div>
-         <div className="d-flex flex-row justify-content-evenly grid-wrapper">
-            <Card
-               src="/images/SignUp.svg"
-               title="SIGN UP"
-               description="Create an account, choose your game, invite friends to join your
+    return (
+        <div className="works p-4">
+            <div className="title">
+                <h1>HOW GCC WORKS</h1>
+            </div>
+            <div className="row card-grid mx-auto">
+                <Card
+                    src="/images/SignUp.svg"
+                    title="SIGN UP"
+                    description="Create an account, choose your game, invite friends to join your
             team, and join the game tournaments"
-            />
-            <Card
-               src="/images/Contest.svg"
-               title="CONTEST"
-               description="Join the game with the terms and condition of the tournament
+                />
+                <Card
+                    src="/images/Contest.svg"
+                    title="CONTEST"
+                    description="Join the game with the terms and condition of the tournament
                host and compete against pros to become the champion of the
                game"
-            />
-            <Card
-               src="/images/Prizes.svg"
-               title="PRIZES"
-               description="Get your prizes by winning the game against pros from the
+                />
+                <Card
+                    src="/images/Prizes.svg"
+                    title="PRIZES"
+                    description="Get your prizes by winning the game against pros from the
                tournament"
-            />
-         </div>
-         <div className="learnMore">
-            <a href="/help">Learn More About GCC ...</a>
-         </div>
-      </div>
-   );
+                />
+            </div>
+            <div className="learnMore">
+                <a href="/help">Learn More About GCC ...</a>
+            </div>
+        </div>
+    );
 };
 
 export default works;
