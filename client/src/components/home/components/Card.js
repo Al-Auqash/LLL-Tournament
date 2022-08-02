9382d1b0d08d994d4b19f954a000526151dc5b62
Al-Auqash <ilhamtawakhal@gmail.com>
2022-08-02 21:38:38 +0700

    #63 recolor

--- client/src/components/home/components/Card.js
+++ client/src/components/home/components/Card.js
@@ -3,7 +3,7 @@ import "../Homepage.css";
 
 const Card = (props) => {
    return (
-      <div className="card cs-card p-4 text-center">
+      <div className="card cs-card py-4 text-center">
          <img src={props.src} width="100"></img>
          <div className="card-body">
             <h2 className="card-title">{props.title}</h2>
