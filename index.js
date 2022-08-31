3070ec48eaa09ba077bb4f5adaf03c055d7d82e3
Al-Auqash <ilhamtawakhal@gmail.com>
2022-08-31 21:16:51 +0700

    #102 env status

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // database
 database();
 
-// if (
-//    process.env.NODE_ENV === "production" ||
-//    process.env.NODE_ENV === "staging"
-// ) {
-   // app.use(express.static("client/build"));
-   // app.get("*", (req, res) => {
-   //    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-   // });
-// }
+if (
+   process.env.NODE_ENV === "production" ||
+   process.env.NODE_ENV === "staging"
+) {
+   app.use(express.static("client/build"));
+   app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/build/index.html"));
+   });
+}
 
 const authentication = require("./routes/authentication");
 const tournament = require("./routes/tournament");
