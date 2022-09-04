31fcd521b5b5c058556c25c23f3af62a05bd027a
Al-Auqash <64571752+Al-Auqash@users.noreply.github.com>
2022-09-04 20:59:32 +0700

    #116 class component to function component

--- client/src/components/tournament/Tournament.js
+++ client/src/components/tournament/Tournament.js
@@ -1,164 +1,190 @@
-import React, { Component, useEffect, useState } from "react";
+import React, {Component, useEffect, useState} from "react";
 import {
-   BrowserRouter as Router,
-   Switch,
-   Route,
-   Link,
-   NavLink,
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    NavLink,
 } from "react-router-dom";
 import axios from "axios";
 import "./Tournament.css";
 // import BaseUrl from "./../baseUrl";
 
-class Tournament extends Component {
-   // const Tournament = () => {
-   constructor() {
-      super();
+// class Tournament extends Component {
+//    constructor() {
+//       super();
+//
+//       this.state = {
+//          Tournaments: [],
+//          TournamentsFinished: [],
+//       };
+//    }
 
-      this.state = {
-         Tournaments: [],
-         TournamentsFinished: [],
-      };
-   }
 
-   componentDidMount() {
-      axios
-         .get(process.env.REACT_APP_URL + "tournament/api")
-         .then((response) => {
-            this.setState({
-               Tournaments: response.data,
-            });
-            console.log(this.state.Tournaments);
-         })
-         .catch((error) => {
-            console.log(error);
-         });
+// componentDidMount() {
+//    axios
+//       .get(process.env.REACT_APP_URL + "tournament/api")
+//       .then((response) => {
+//          this.setState({
+//             Tournaments: response.data,
+//          });
+//          console.log(this.state.Tournaments);
+//       })
+//       .catch((error) => {
+//          console.log(error);
+//       });
+//
+//    axios
+//       .get(process.env.REACT_APP_URL + "tournament/api/finished")
+//       .then((response) => {
+//          this.setState({
+//             TournamentsFinished: response.data,
+//          });
+//       })
+//       .catch((error) => {
+//          console.log(error);
+//       });
+// }
 
-      axios
-         .get(process.env.REACT_APP_URL + "tournament/api/finished")
-         .then((response) => {
-            this.setState({
-               TournamentsFinished: response.data,
-            });
-         })
-         .catch((error) => {
-            console.log(error);
-         });
-   }
+const Tournament = () => {
+    const [tournaments, setTournaments] = useState([])
+    const [finishedTournaments, setFinishedTournaments] = useState([])
+    const getTournaments = () => {
+        axios.get(process.env.REACT_APP_URL + 'tournament/api')
+            .then((response) => {
+                setTournaments(response.data)
+            }).catch((error) => [
+            console.log(error)
+        ])
+    }
+    const getFinishedTournaments = () => {
+        axios.get(process.env.REACT_APP_URL + 'tournament/api/finished')
+            .then((response) => {
+                setFinishedTournaments(response.data)
+            }).catch((error) => [
+            console.log(error)
+        ])
+    }
 
-   // const [tourney, setTourney] = useState([]);
+    useEffect(() => {
+        getTournaments();
+    },[])
 
-   // useEffect(() => {
-   //   const fetchData = async () => {
-   //     const result = await fetch(`http://localhost:5000/tournament/api`);
-   //     const body = await result.json();
-   //     console.log(body);
-   //     setTourney(body);
-   //   };
-   //   fetchData();
-   // }, []);
 
-   render() {
-      const { Tournaments } = this.state;
-      const { TournamentsFinished } = this.state;
-      return (
-         <div id="tournament">
-            <div className="tournamentLeftContent">
-               <div className="subLeftContent">
-                  <div className="gameMenu">
-                     <img
-                        className="imgGameMenu"
-                        src="/images/apex-logo.png"
-                     ></img>
-                     {/* <img className="imgGameMenu" src={valorant}></img> */}
-                  </div>
-                  <div className="mainContent">
-                     <div className="contentMenu">
-                        <NavLink
-                           exact
-                           to="/tournament"
-                           activeStyle={{ textDecoration: "underline" }}
-                        >
-                           ACTIVE TOURNAMENT
-                        </NavLink>
-                        <a>|</a>
-                        <NavLink
-                           exact
-                           to="/tournament/finished"
-                           activeStyle={{ textDecoration: "underline" }}
-                        >
-                           FINISHED TOURNAMENT
-                        </NavLink>
-                     </div>
-                     <Router>
-                        <Switch>
-                           <Route exact path="/tournament">
-                              <div className="tournamentTable">
-                                 {Tournaments?.length ? (
-                                    Tournaments?.map((Tournaments) => (
-                                       <Link
-                                          className="tournamentTableContent"
-                                          to="/help"
-                                          key={Tournaments.id}
-                                       >
-                                          <h3>
-                                             {Tournaments.name.toUpperCase()}
-                                          </h3>
-                                          <h3>{Tournaments.status}</h3>
-                                          <h3>{Tournaments.prize}</h3>
-                                       </Link>
-                                    ))
-                                 ) : (
-                                    <div className="tournamentTableContent">
-                                       {" "}
-                                       <h3>No Tournament Found</h3>
-                                    </div>
-                                 )}
-                              </div>
-                           </Route>
-                           <Route exact path="/tournament/finished">
-                              <div className="tournamentTable">
-                                 {TournamentsFinished?.length ? (
-                                    TournamentsFinished?.map((Tournaments) => (
-                                       <Link
-                                          className="tournamentTableContent"
-                                          to="/help"
-                                          key={Tournaments.id}
-                                       >
-                                          <h3>
-                                             {Tournaments.name.toUpperCase()}
-                                          </h3>
-                                          <h3>{Tournaments.status}</h3>
-                                          <h3>{Tournaments.prize}</h3>
-                                       </Link>
-                                    ))
-                                 ) : (
-                                    <div className="tournamentTableContent">
-                                       {" "}
-                                       <h3>No Tournament Found</h3>
-                                    </div>
-                                 )}
-                              </div>
-                           </Route>
-                        </Switch>
-                     </Router>
-                  </div>
-               </div>
+    // const [tourney, setTourney] = useState([]);
+
+    // useEffect(() => {
+    //   const fetchData = async () => {
+    //     const result = await fetch(`http://localhost:5000/tournament/api`);
+    //     const body = await result.json();
+    //     console.log(body);
+    //     setTourney(body);
+    //   };
+    //   fetchData();
+    // }, []);
+
+    // render()
+    // {
+    //     const {Tournaments} = this.state;
+    //     const {TournamentsFinished} = this.state;
+        return (
+            <div id="tournament">
+                <div className="tournamentLeftContent">
+                    <div className="subLeftContent">
+                        <div className="gameMenu">
+                            <img
+                                className="imgGameMenu"
+                                src="/images/apex-logo.png"
+                            ></img>
+                            {/* <img className="imgGameMenu" src={valorant}></img> */}
+                        </div>
+                        <div className="mainContent">
+                            <div className="contentMenu">
+                                <NavLink
+                                    exact
+                                    to="/tournament"
+                                    activeStyle={{textDecoration: "underline"}}
+                                >
+                                    ACTIVE TOURNAMENT
+                                </NavLink>
+                                <a>|</a>
+                                <NavLink
+                                    exact
+                                    to="/tournament/finished"
+                                    activeStyle={{textDecoration: "underline"}}
+                                >
+                                    FINISHED TOURNAMENT
+                                </NavLink>
+                            </div>
+                            <Router>
+                                <Switch>
+                                    <Route exact path="/tournament">
+                                        <div className="tournamentTable">
+                                            {tournaments?.length ? (
+                                                tournaments.map((Tournaments) => (
+                                                    <Link
+                                                        className="tournamentTableContent"
+                                                        to="/help"
+                                                        key={Tournaments.id}
+                                                    >
+                                                        <h3>
+                                                            {Tournaments.name.toUpperCase()}
+                                                        </h3>
+                                                        <h3>{Tournaments.status}</h3>
+                                                        <h3>{Tournaments.prize}</h3>
+                                                    </Link>
+                                                ))
+                                            ) : (
+                                                <div className="tournamentTableContent">
+                                                    {" "}
+                                                    <h3>No Tournament Found</h3>
+                                                </div>
+                                            )}
+                                        </div>
+                                    </Route>
+                                    <Route exact path="/tournament/finished">
+                                        <div className="tournamentTable">
+                                            {finishedTournaments?.length ? (
+                                                finishedTournaments.map((Tournaments) => (
+                                                    <Link
+                                                        className="tournamentTableContent"
+                                                        to="/help"
+                                                        key={Tournaments.id}
+                                                    >
+                                                        <h3>
+                                                            {Tournaments.name.toUpperCase()}
+                                                        </h3>
+                                                        <h3>{Tournaments.status}</h3>
+                                                        <h3>{Tournaments.prize}</h3>
+                                                    </Link>
+                                                ))
+                                            ) : (
+                                                <div className="tournamentTableContent">
+                                                    {" "}
+                                                    <h3>No Tournament Found</h3>
+                                                </div>
+                                            )}
+                                        </div>
+                                    </Route>
+                                </Switch>
+                            </Router>
+                        </div>
+                    </div>
+                </div>
+                <div className="tournamentRightContent">
+                    <h1>
+                        ARE YOU BRAVE ENOUGH TO SHOW THE WORLD WHO IS THE CHAMPION?
+                        SIGN UP NOW!
+                    </h1>
+                    <Link to="/signUp">
+                        <button className="joinNow">
+                            <p className="btnText">Join Now</p>
+                        </button>
+                    </Link>
+                </div>
             </div>
-            <div className="tournamentRightContent">
-               <h1>
-                  ARE YOU BRAVE ENOUGH TO SHOW THE WORLD WHO IS THE CHAMPION?
-                  SIGN UP NOW!
-               </h1>
-               <Link to="/signUp">
-                  <button className="joinNow">
-                     <p className="btnText">Join Now</p>
-                  </button>
-               </Link>
-            </div>
-         </div>
-      );
-   }
+        );
+    // }
 }
 
 export default Tournament;
